4058a0d3d814df2c20d099c411272ea74827ab93
Justin <yustinyakel100@gmail.com>
2024-06-06 15:25:45 -0400

    Created model to predict a spiral of data points

--- learnSpirals.py
+++ learnSpirals.py
@@ -7,6 +7,10 @@ from sklearn.model_selection import train_test_split
 import helper_funcs as helper
 import matplotlib.pyplot as plt
 
+RAND_SEED: Final[int] = 50
+LR: Final[float] = 0.01  #If using 0.1, turn epochs down to ~10,000 otherwise accuracy starts decreasing / loss increases
+EPOCHS: Final[int] = 25000
+
 #Code to generate sprials from here: https://cs231n.github.io/neural-networks-case-study/
 
 N = 100 # number of points per class
@@ -21,6 +25,72 @@ for j in range(K):
   X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
   y[ix] = j
 
+input_X = torch.from_numpy(X).type(torch.float)
+input_y = torch.from_numpy(y).type(torch.float)
+
+x_train, x_test, y_train, y_test = train_test_split(input_X, input_y, test_size=0.2, random_state=RAND_SEED)
+
 helper.createInputImage(X, y, name="spiral_input.png")
 
 device = helper.determineDevice()
+
+class SpiralModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units=8):
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            nn.ReLU(), 
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_features),
+        )
+
+    def forward(self, x):
+       return self.linear_layer_stack(x)
+
+#Input is x,y coords so 2, and output is number of classes, which is 6
+spiral_model_0 = SpiralModel(input_features = 2, output_features = 6, hidden_units=32).to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(spiral_model_0.parameters(), LR)
+
+# Ensure all data is on the same device
+x_train, y_train = x_train.to(device), y_train.to(device)
+x_test, y_test = x_test.to(device), y_test.to(device)
+
+# We use CrossEntropyLoss for the loss function, so we need to use longs instead of floats
+y_train = y_train.to(torch.long)
+y_test = y_test.to(torch.long)
+
+torch.manual_seed(RAND_SEED)
+
+for epoch in range(EPOCHS + 1):
+    ### TRAINING
+    spiral_model_0.train()
+
+    y_logits = spiral_model_0(x_train) # model outputs raw logits 
+    y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1) # go from logits -> prediction probabilities -> prediction labels
+
+    loss = loss_fn(y_logits, y_train) 
+    accuracy = helper.accuracy_fn(y_train, y_pred)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    ### TESTING
+    spiral_model_0.eval()
+    with torch.inference_mode():    
+        test_logits = spiral_model_0(x_test) 
+        test_pred = torch.softmax(test_logits, dim=1).argmax(dim=1)
+
+        test_loss = loss_fn(test_logits, y_test)
+        test_accuracy = helper.accuracy_fn(y_test, test_pred)
+
+    if epoch % 2500 == 0:
+        # Print out current status of the model
+        print(f"Epoch: {epoch} | Accuracy: {accuracy: 0.2f}% | Loss: {test_loss: 0.5f} | - | Test Accuracy: {test_accuracy: 0.2f}% | Test Loss: {test_loss: 0.5f}")
+
+helper.createOutputImage(spiral_model_0, x_train, y_train, x_test, y_test, name="spiral_output.png" )
