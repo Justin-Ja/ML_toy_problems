2c144cf4b9d96ed5d9b27c21e8cca2f69ed3c22f
Justin <yustinyakel100@gmail.com>
2024-06-06 11:31:20 -0400

    Added more helper functions

--- helper_funcs.py
+++ helper_funcs.py
@@ -3,7 +3,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 
+# Creates a matplotlib plot of the input moons and save the image
 def createMoonInputImage(x_values, y_values, name="moon_input.png"):
     plt.figure(figsize=(10, 7))
     plt.scatter(x_values[:, 0], x_values[:, 1], c=y_values, cmap=plt.cm.jet)
     plt.savefig(name)
+
+# Create a matplotlib plot of the model's results of the training and test data points
+def createMoonOutputImage(model, x_train, y_train, x_test, y_test, name="moon_output.png"):
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Train")
+    plotDecisionBoundary(model, x_train, y_train)
+    plt.subplot(1, 2, 2)
+    plt.title("Test")
+    plotDecisionBoundary(model, x_test, y_test)
+    plt.savefig(name)
+
+# Creates a white boundary line between group areas indentified by the model
+def plotDecisionBoundary(model, x_values, y_values):
+
+    model.to("cpu")
+    x_values, y_values = x_values.to("cpu"), y_values.to("cpu")
+
+    # Setup prediction boundaries and grid
+    x_min, x_max = x_values[:, 0].min() - 0.1, x_values[:, 0].max() + 0.1
+    y_min, y_max = x_values[:, 1].min() - 0.1, x_values[:, 1].max() + 0.1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), np.linspace(y_min, y_max, 101))
+
+    # Make features
+    X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).float()
+
+    # Make predictions
+    model.eval()
+    with torch.inference_mode():
+        y_logits = model(X_to_pred_on)
+
+    # Test for multi-class or binary and adjust logits to prediction labels
+    if len(torch.unique(y_values)) > 2:
+        y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)  # mutli-class
+    else:
+        y_pred = torch.round(torch.sigmoid(y_logits))  # binary
+
+    # Reshape preds and plot
+    y_pred = y_pred.reshape(xx.shape).detach().numpy()
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.jet, alpha=0.7)
+    plt.scatter(x_values[:, 0], x_values[:, 1], c=y_values, s=40, cmap=plt.cm.jet)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+
+# Calculates the accuracy of the model
+def accuracy_fn(input_values, predicted_values):
+    correctGuesses = torch.eq(input_values, predicted_values).sum().item()
+    return ((correctGuesses / len(predicted_values)) * 100)
+
