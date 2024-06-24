5af3f647961e26768b79c632b586de24ca0ff7a8
Justin <yustinyakel100@gmail.com>
2024-06-24 10:55:55 -0400

    Added command line arguments

--- learnSpirals.py
+++ learnSpirals.py
@@ -5,18 +5,39 @@ from typing import Final
 from sklearn.model_selection import train_test_split
 import helperFuncs as helper
 from timeit import default_timer as timer
+import argparse
 
-RAND_SEED: Final[int] = 50
-HIDDEN_UNITS: Final[int] = 32
-LR: Final[float] = 0.01  #If using 0.1, turn epochs down to ~10,000 otherwise accuracy starts decreasing / loss increases (overfitting?)
-EPOCHS: Final[int] = 25000
-THETA: Final[float] = 0.175
+parser = argparse.ArgumentParser(prog="\nA ML program that trains to learn a multi-class classification problem\n")
 
+parser.add_argument('-N', '--samples', help='Number of points PER CLASS to generate', 
+                    default=100, type=int)
+parser.add_argument('-c', '--classes', help='Number of groups/classes to create when generating the spiral', 
+                    default=4, type=int)
+parser.add_argument('-t', '--theta', help='A value between 0 and 1 of how spread out the points are from their \"central line". A higher value means points are further apart.', 
+                    default=0.175, type=float)
+parser.add_argument('-e', '--epochs', help='The number of loops should the model train/test for', 
+                    default=20000, type=int)
+parser.add_argument('-l', '--learning_rate', help='The rate that the optimizer will update the model\'s parameters at.', 
+                    default=0.01, type=float)
+parser.add_argument('-u', '--units', help='Number of neurons to use per ML layer. Higher value helps the model be more accurate, at the cost of training time\n', 
+                    default=32, type=int)
+parser.add_argument('-s', '--seed', help='Value of seed for RNG', 
+                    default=42, type=int)
+
+args = parser.parse_args()
+print(args)
+
+RAND_SEED: Final[int] = args.seed
+HIDDEN_UNITS: Final[int] = args.units
+LR: Final[float] = args.learning_rate  
+EPOCHS: Final[int] = args.epochs
+THETA: Final[float] = args.theta
+print(LR)
 #Code to generate sprials from here: https://cs231n.github.io/neural-networks-case-study/
 
-N = 100 # number of points per class
+N = args.samples # number of points per class
 D = 2 # dimensionality
-K = 6 # number of classes
+K = args.classes # number of classes
 X = np.zeros((N*K,D)) # data matrix (each row = single example)
 y = np.zeros(N*K, dtype='uint8') # class labels
 for j in range(K):
@@ -64,9 +85,11 @@ x_test, y_test = x_test.to(device), y_test.to(device)
 # We use CrossEntropyLoss for the loss function, so we need to use longs instead of floats
 y_train = y_train.to(torch.long)
 y_test = y_test.to(torch.long)
-
 torch.manual_seed(RAND_SEED)
+torch.cuda.manual_seed(RAND_SEED)
 start = timer()
+
+modulusPrintValue = int(EPOCHS / 10)
 
 for epoch in range(EPOCHS + 1):
     ### TRAINING
@@ -91,7 +114,7 @@ for epoch in range(EPOCHS + 1):
         test_loss = loss_fn(test_logits, y_test)
         test_accuracy = helper.accuracy_fn(y_test, test_pred)
 
-    if epoch % 2500 == 0:
+    if epoch != 0 and epoch % modulusPrintValue == 0:
         # Print out current status of the model
         print(f"Epoch: {epoch} | Accuracy: {accuracy: 0.2f}% | Loss: {test_loss: 0.5f} | - | Test Accuracy: {test_accuracy: 0.2f}% | Test Loss: {test_loss: 0.5f}")
 
